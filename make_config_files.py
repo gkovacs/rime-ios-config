57421eed311ed9e165c5f5f47da2a6cf2ed028c2
Geza Kovacs <geza0kovacs@gmail.com>
2023-11-26 23:21:17 -0800

    added keyboard shortcut

--- make_config_files.py
+++ make_config_files.py
@@ -27,6 +27,7 @@ x['schema']['dependencies'].remove('terra_pinyin_simp')
 x['schema']['dependencies'].remove('td_pinyin_flypy_simp')
 x['putonghua_to_cangjie5_lookup']['dictionary'] = 'terra_pinyin'
 x['putonghua_to_cangjie5_lookup']['prism'] = 'td_pinyin_flypy'
+x['key_binder']['bindings'][0]['select'] = 'double_jyutping_ext'
 yaml.dump(x, Path('double_pinyin_flypy_show_jyutping.schema.yaml'))
 
 extra = yaml.load(Path('show_tones_base.yaml'))
@@ -51,6 +52,7 @@ x['putonghua_to_tones_reverse_lookup'] = extra['putonghua_to_tones_reverse_looku
 #del x['recognizer']['patterns']['putonghua_to_jyutping_lookup']
 x['recognizer']['patterns']['putonghua_to_tones_lookup'] = "^[a-z]*$"
 x['switches'][2]['reset'] = 1
+x['key_binder']['bindings'][0]['select'] = 'double_jyutping_shumianyu_simp_ext'
 yaml.dump(x, Path('double_pinyin_flypy_simp_show_tones.schema.yaml'))
 
 x['schema']['name'] = '雙拼顯調'
@@ -60,6 +62,7 @@ x['schema']['dependencies'].remove('terra_pinyin_simp')
 x['schema']['dependencies'].remove('td_pinyin_flypy_simp')
 x['putonghua_to_cangjie5_lookup']['dictionary'] = 'terra_pinyin'
 x['putonghua_to_cangjie5_lookup']['prism'] = 'td_pinyin_flypy'
+x['key_binder']['bindings'][0]['select'] = 'double_jyutping_shumianyu_ext'
 yaml.dump(x, Path('double_pinyin_flypy_show_tones.schema.yaml'))
 
 x = yaml.load(Path('double_jyutping_simp_ext.schema.yaml'))
@@ -70,6 +73,7 @@ x['schema']['dependencies'].remove('jyut6ping3_simp')
 x['schema']['dependencies'].remove('double_jyutping_simp')
 x['jyutping_to_cangjie5_lookup']['dictionary'] = 'jyut6ping3'
 x['jyutping_to_cangjie5_lookup']['prism'] = 'double_jyutping'
+x['key_binder']['bindings'][0]['select'] = 'double_pinyin_flypy_show_jyutping'
 yaml.dump(x, Path('double_jyutping_ext.schema.yaml'))
 
 x = yaml.load(Path('double_jyutping_simp_ext.schema.yaml'))
@@ -80,6 +84,7 @@ replace_in_list(x['schema']['dependencies'], 'double_jyutping', 'double_jyutping
 x['translator']['dictionary'] = 'jyut6ping3_shumianyu'
 x['translator']['prism'] = 'double_jyutping_shumianyu'
 x['switches'][2]['reset'] = 1
+x['key_binder']['bindings'][0]['select'] = 'double_pinyin_flypy_simp_show_tones'
 yaml.dump(x, Path('double_jyutping_shumianyu_simp_ext.schema.yaml'))
 
 x['schema']['name'] = '粵雙拼書'
@@ -89,4 +94,5 @@ x['schema']['dependencies'].remove('jyut6ping3_simp')
 x['schema']['dependencies'].remove('double_jyutping_simp')
 x['jyutping_to_cangjie5_lookup']['dictionary'] = 'jyut6ping3'
 x['jyutping_to_cangjie5_lookup']['prism'] = 'double_jyutping'
+x['key_binder']['bindings'][0]['select'] = 'double_pinyin_flypy_show_tones'
 yaml.dump(x, Path('double_jyutping_shumianyu_ext.schema.yaml'))
